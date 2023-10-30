db6f789b4f2fa81a6495565d61d05f471029dbd1
Alan Ribeiro da Silva <Alan.Ribeiro@lbv.org.br>
2023-10-30 11:16:35 -0300

    adicionando lista-duas-dimensoes.js

--- js/sala/lista-duas-dimensoes.js
+++ js/sala/lista-duas-dimensoes.js
@@ -2,3 +2,5 @@ const alunos = ["João", "Juliana", "Ana", "Caio"];
 const medias = [10, 8, 7.5, 9];
 
 const listaAlunosMedias = [alunos, medias]
+
+console.log(`A aluna que está na posição 1 na lista de alunos é: ${listaAlunosMedias[0][3]}`);
